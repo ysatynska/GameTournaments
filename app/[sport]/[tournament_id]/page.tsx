4e365701a208b56ecde93763701bd7b71a67bb6e
ysatynska <ysatynska@mail.roanoke.edu>
2024-11-20 16:07:02 -0500

    bleh

---
+++ app/[sport]/[tournament_id]/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function TournamentPage() {
+  return (
+    <div>
+      <h1 className={title()}>Tournament</h1>
+    </div>
+  );
+}
