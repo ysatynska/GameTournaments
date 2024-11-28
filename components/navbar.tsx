39ac5a41d890173b08e18ef59ca924ab82691399
ysatynska <ysatynska@mail.roanoke.edu>
2024-11-27 19:04:19 -0500

    vercel

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -80,7 +80,6 @@ export const Navbar = () => {
                   )}
                   as={NextLink}
                   href={item.href}
-                  disableRipple
                 >
                   {item.label}
                 </Link>
