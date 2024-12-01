0f5d95c1cf21a5318ed23e706db0e12b1eebbe32
ysatynska <ysatynska@mail.roanoke.edu>
2024-12-01 10:50:09 -0500

    vercel

--- app/my_games/page.tsx
+++ app/my_games/page.tsx
@@ -4,9 +4,9 @@ import GamesTable from '@/components/ui/games';
 import { Suspense } from 'react';
 
 export default function MyGames () {
-  const searchParams = useSearchParams();
-  const player_id = searchParams.get('player_id');
-  const sport_id = searchParams.get('sport_id');
+  // const searchParams = useSearchParams();
+  // const player_id = searchParams.get('player_id');
+  // const sport_id = searchParams.get('sport_id');
 
   return (
     // <Suspense fallback={<p>Loading games...</p>}>
