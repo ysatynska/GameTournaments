659f01a2eb93ced236ba8787518af324994d030b
ysatynska <ysatynska@mail.roanoke.edu>
2024-12-09 20:14:33 -0500

    fix

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/components/ui/login-form';
  
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center h-[calc(100vh-4.2rem)]">
+    <main className="flex items-center justify-center h-[calc(100vh-4.15rem)]">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4">
         <LoginForm />
       </div>
